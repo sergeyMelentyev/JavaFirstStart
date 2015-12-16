7396d461f85e9e6249f0436b84db2184b735d1ee
sergeyMelentyev <sergey.melentyev@gmail.com>
2015-12-16 15:47:50 +0300

    new version

--- src/newpackage/Start.java
+++ src/newpackage/Start.java
@@ -3,19 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javafirststart;
+package newpackage;
 
 /**
  *
  * @author olgarezchikova
  */
-public class JavaFirstStart {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello, World");
-    }
-    
+public class Start {
+    private String color = "Red";
+    private int roomCount = 5;
 }
